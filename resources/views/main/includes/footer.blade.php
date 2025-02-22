3013c192f909471f7224ce1b9c75d6626911eccd
MNIH <mnih550@gmail.com>
2025-02-22 20:37:32 +0100

    Welcome page has been added

---
+++ resources/views/main/includes/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="w-full h-[400px] bg-bg flex items-center justify-between px-16">
+
+    <div class="flex flex-col justify-center gap-1 text-white font-light">
+        <h1 class="text-2xl font-extrabold">VetFinder</h1>
+        <p>ul. Ulicowa 15</p>
+        <p>Miastowo, 00-000</p>
+        <a href="tel:+00000000000"><i class="fa-solid fa-phone mr-2"></i>+00 000 000 000</a>
+        <a href="mailto:contact@vetfinder.pl"><i class="fa-solid fa-envelope mr-2"></i>contact@vetfinder.pl</a>
+        <hr class="w-[500px] h-[2px] bg-primary border-none">
+        <p>Copyright ©2025 | Projekt stworzony przez <a href="https://github.com/mnih1/" target="_blank">Michał “MNIH” Faber</a></p>
+    </div>
+
+    <div class="flex text-white font-light">
+        <ul class="border-r border-white px-3 flex flex-col gap-1">
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Jak działa serwis</a></li>
+            <li><a href="#">Szukaj specjalisty</a></li>
+            <li><a href="#">Najwyżej oceniani</a></li>
+            <li><a href="#">Dołącz do projektu</a></li>
+        </ul>
+        <ul class="px-3 flex flex-col gap-1">
+            <li><a href="#">Kontakt</a></li>
+            <li><a href="#">Pomoc techniczna</a></li>
+            <li><a href="#">O projekcie</a></li>
+            <li><a href="#">O nas</a></li>
+            <li><a href="#">Polityka prywatności</a></li>
+        </ul>
+    </div>
+
+</footer>
---
+++ resources/views/main/includes/nav.blade.php
@@ -0,0 +1,22 @@
+<nav class="w-full h-[150px]">
+
+    <div class="bg-bg w-full h-[50px]"></div>
+    <div class="w-full h-[100px] flex items-center justify-between px-16">
+        <h1 class="text-3xl font-extrabold">Vetfinder</h1>
+        <div class="flex items-center">
+            {{-- TODO: Search from - kiedy zrobione będą wyniki i page --}}
+            <form class="flex items-center" action="" method="post">
+                @csrf
+                <input type="text" name="" class="w-[500px] h-[50px] border-none bg-gray-400 text-white placeholder-white" placeholder="Kogo szukasz?">
+                <button class="w-[50px] h-[50px] bg-primary text-white text-lg group">
+                    <i class="fa-solid fa-magnifying-glass transform transition-transform duration-200 group-hover:scale-125"></i></button>
+                </button>
+            </form>
+            <button class="w-[50px] h-[50px] bg-secondary text-white text-lg group">
+                <i class="fa-solid fa-bars transform transition-transform duration-200 group-hover:scale-125"></i>
+            </button>
+            
+        </div>
+    </div>
+
+</nav>
