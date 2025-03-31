712a5f48e91f7706834f50eea5bd361094eecaea
MNIH <97916633+mnih1@users.noreply.github.com>
2025-03-31 07:58:38 +0200

    Random commit

--- app/Models/Adresses.php
+++ app/Models/Adresses.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Adresses extends Model
 {
-    //
+    use HasFactory, Notifiable;
+
+    protected $table = 'adresses';
+
+    protected $fillable = [
+        'adress_city',
+        'adress_street',
+        'adress_buildingNumber',
+        'adress_apartmentNumber',
+        'postal_code',
+    ];
 }
