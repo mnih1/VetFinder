c3eb6faeccbde827c9174cf7da08b34ee98f4977
MNIH <mnih550@gmail.com>
2025-07-28 23:11:38 +0200

    responsive design for footer, nav and vet results

--- resources/views/main/includes/nav.blade.php
+++ resources/views/main/includes/nav.blade.php
@@ -1,16 +1,21 @@
 <nav class="w-full h-[150px]">
 
     <div class="bg-bg w-full h-[50px]"></div>
-    <div class="w-full h-[100px] flex items-center justify-between px-16">
-        <a href="/" class="h-full text-3xl font-extrabold"><img src="{{ asset('img/VetFinder-logo-text.png') }}"
-                alt="Logo VetFinder" class="h-full"></a>
+    <div class="w-full h-[100px] flex items-center justify-between px-4 md:px-16">
+        <a href="/" class="h-full text-3xl font-extrabold">
+            <img src="{{ asset('img/VetFinder-logo-text.png') }}"
+                alt="Logo VetFinder" class="h-full hidden md:inline">
+            <img src="{{ asset('img/VetFinder-logo.png') }}"
+                alt="Logo VetFinder" class="h-full inline md:hidden">
+            </a>
         <div class="flex items-center">
             {{-- TODO: Search from - kiedy zrobione będą wyniki i page --}}
             <form class="flex items-center" action="" method="post">
                 @csrf
-                <input type="text" name=""
-                    class="w-[500px] h-[50px] border-none bg-gray-500 text-white placeholder-white"
-                    placeholder="Kogo szukasz?">
+                <input type="text" name="" 
+                    class="hidden md:block h-[50px] border-none bg-gray-500 text-white placeholder-white"
+                    placeholder="Kogo szukasz?" />
+
                 <button class="w-[50px] h-[50px] bg-primary text-white text-lg group">
                     <i class="fa-solid fa-magnifying-glass transform transition-transform duration-200 group-hover:scale-125"
                         aria-label="Szukaj"></i></button>
